b45846041252193d98846fef68f27bb110beebec
IngDesarrolloBonnadona <ingdesarrollo@bonnadona.co>
2024-08-28 19:11:14 -0500

    config next auth in progress

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,42 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        email: {
+          label: "Correo",
+          type: "email",
+          pattern: "^[w.-]+@[a-zA-Zd.-]+.[a-zA-Z]{2,}$",
+        },
+        password: { label: "Contraseña ", type: "password" },
+      },
+      async authorize(credentials, req) {
+        if (!credentials) {
+            throw new Error("Credenciales no definidas.");
+          }
+        // Add logic here to look up the user from the credentials supplied
+        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
