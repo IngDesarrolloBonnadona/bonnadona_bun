67569e0e50d33edf2d6355fa4ad04c20db51ebea
IngDesarrolloBonnadona <ingdesarrollo@bonnadona.co>
2024-09-03 18:04:07 -0500

    add new custom components

---
+++ src/components/common/custom_button/CustomButton.tsx
@@ -0,0 +1,58 @@
++"use client";
+
+import React, { ReactNode } from "react";
+
+import { Button } from "antd";
+import { ButtonHTMLType, ButtonType } from "antd/es/button";
+import { SizeType } from "antd/es/config-provider/SizeContext";
+
+const CustomButton: React.FC<{
+  titleCustomButton?: string;
+  typeCustomButton: ButtonType;
+  htmlTypeCustomButton?: ButtonHTMLType;
+  idCustomButton?: string | undefined;
+  classNameCustomButton?: string;
+  sizeCustomButton: SizeType;
+  styleCustomButton: React.CSSProperties | undefined;
+  iconCustomButton?: ReactNode;
+  iconPositionCustomButton?: "start" | "end";
+  formCustomButton?: string | undefined;
+  disabledCustomButton?: boolean
+  onClickCustomButton: () => void;
+  onMouseDownCustomButton?: () => void;
+}> = ({
+  titleCustomButton,
+  typeCustomButton,
+  htmlTypeCustomButton,
+  idCustomButton,
+  classNameCustomButton,
+  sizeCustomButton,
+  styleCustomButton,
+  iconCustomButton,
+  iconPositionCustomButton,
+  formCustomButton,
+  disabledCustomButton,
+  onClickCustomButton,
+  onMouseDownCustomButton,
+}) => {
+  return (
+    <Button
+      id={idCustomButton}
+      className={classNameCustomButton}
+      type={typeCustomButton}
+      size={sizeCustomButton}
+      htmlType={htmlTypeCustomButton || "button"}
+      style={styleCustomButton}
+      icon={iconCustomButton}
+      iconPosition={iconPositionCustomButton}
+      form={formCustomButton}
+      disabled={disabledCustomButton}
+      onClick={onClickCustomButton}
+      onMouseDown={onMouseDownCustomButton}
+    >
+      {titleCustomButton}
+    </Button>
+  );
+};
+
+export default CustomButton;
