a9e8434085aea9a7e2d09b659f2c74e88136fba6
IngDesarrolloBonnadona <ingdesarrollo@bonnadona.co>
2024-09-09 19:09:36 -0500

    A carousel component is added to the right to display images

--- src/components/auth/form/LoginUserForm.tsx
+++ src/components/auth/form/LoginUserForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import { Form, Input, Button, Typography } from "antd";
+import { Form, Input, Button, Typography, Col, Carousel, Row, Image } from "antd";
 import { UserOutlined, KeyOutlined } from "@ant-design/icons";
 import TwoFactorAuthModal from "../modal/TwoFactorAuthModal";
 
@@ -33,6 +33,13 @@ const LoginUserForm = () => {
     console.log("Código verificado:", code);
     setIsModalVisible(false);
   };
+
+  const imagesCarousel = [
+    "/logos/LOGO-BONNADONA-2.png",
+    "/logos/LOGO-BONNADONA-HUB-2.png",
+    "/logos/LOGO-BONNADONA.png"
+
+  ]
 
   return (
     <div
@@ -69,101 +76,156 @@ const LoginUserForm = () => {
           backgroundColor: "rgba(255, 255, 255, 0.4)",
         }}
       />
-      <div
-        style={{
-          backgroundColor: "rgba(255, 255, 255, 1)",
-          padding: "2rem",
-          borderRadius: "20px",
-          boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
-          maxWidth: "400px",
-          width: "100%",
-          position: "relative",
-          zIndex: 1,
-        }}
-      >
-        <div
-          className="bonnadona-hub-logo fade-in"
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            paddingBlock: "1px",
-          }}
-        >
-          <img
-            src="/logos/LOGO-BONNADONA-HUB.png"
-            alt="Logo de Bonnadona HUB"
-            style={{ height: 100, marginBottom: 30 }}
-          />
-        </div>
-        <Title
-          style={{
-            textAlign: "center",
-            fontWeight: "normal",
-            lineHeight: 1.3,
-            marginBottom: "2rem",
-          }}
-          level={2}
-        >
-          Iniciar sesión
-        </Title>
-        <Form onFinish={handleSubmit}>
-          <Form.Item
-            name="email"
-            rules={[
-              { required: true, message: "Por favor ingrese su correo" },
-              // { type: "email", message: "Por favor ingrese un correo válido" },
-              {
-                pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-                message: "Por favor ingrese un correo válido",
-              },
-            ]}
+      <Row gutter={[22, 32]} style={{ width: "100%", maxWidth: "1000px" }}>
+        <Col span={12} style={{ backgroundColor: "#0085c8", borderRadius: "20px" }}>
+          <div style={{ padding: "2rem" }}>
+            <Carousel autoplay arrows fade>
+              {imagesCarousel.map((image, index) => (
+                <div key={index}>
+                  <img
+                    src={image}
+                    alt={`Imagen ${index + 1}`}
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      marginTop: "16px"
+                    }}
+                  />
+                </div>
+              ))}
+              {/* <div>
+                <h3>
+                  <span style={{ fontSize: "1.5rem", fontWeight: "bold" }}>
+                    ¡Hola!{" "}
+                  </span>
+                  <br />
+                  Bienvenido a nuestra plataforma.
+                </h3>
+                <p>Descubre todo lo que podemos hacer por ti.</p>
+              </div>
+              <div>
+                <h3>
+                  <span style={{ fontSize: "1.5rem", fontWeight: "bold" }}>
+                    Beneficios{" "}
+                  </span>
+                  <br />
+                  Disfruta de acceso exclusivo a funciones premium.
+                </h3>
+                <p>Mejora tu experiencia con nosotros.</p>
+              </div>
+              <div>
+                <h3>
+                  <span style={{ fontSize: "1.5rem", fontWeight: "bold" }}>
+                    Soporte{" "}
+                  </span>
+                  <br />
+                  Contacta con nuestro equipo de soporte técnico.
+                </h3>
+                <p>Siempre a tu disposición.</p>
+              </div> */}
+            </Carousel>
+          </div>
+        </Col>
+        <Col span={12}>
+          <div
+            style={{
+              backgroundColor: "rgba(255, 255, 255, 1)",
+              padding: "2rem",
+              borderRadius: "20px",
+              boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
+              maxWidth: "400px",
+              width: "100%",
+              position: "relative",
+              zIndex: 1,
+            }}
           >
-            <Input
-              className="custom-input"
-              type="email"
-              placeholder="Correo"
-              autoComplete="off"
-              prefix={<UserOutlined style={{ color: "rgba(0,0,0,.25)" }} />}
-              value={email}
-              style={{ borderRadius: "30px" }}
-              onChange={handleEmailChange}
-            />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            rules={[
-              { required: true, message: "Por favor ingrese su contraseña" },
-              // { pattern: /^[0-9]+$/, message: "El correo es incorrecto" },
-            ]}
-          >
-            <Input.Password
-              className="custom-input"
-              prefix={<KeyOutlined style={{ color: "rgba(0,0,0,.25)" }} />}
-              value={password}
-              placeholder="Contraseña"
-              style={{ borderRadius: "30px" }}
-              onChange={handlePasswordChange}
-            />
-          </Form.Item>
-          <Form.Item>
-            <a href="#">¿Olvidaste tu contraseña?</a>
-          </Form.Item>
-          <Form.Item style={{ textAlign: "center" }}>
-            <Button
-              type="primary"
-              htmlType="submit"
-              className="login-button"
+            <div
+              className="bonnadona-hub-logo fade-in"
               style={{
-                borderRadius: "30px",
-                textAlign: "center",
-                backgroundColor: "#0085c8",
+                display: "flex",
+                justifyContent: "center",
+                paddingBlock: "1px",
               }}
             >
-              Iniciar Sesión
-            </Button>
-          </Form.Item>
-        </Form>
-      </div>
+              <img
+                src="/logos/LOGO-BONNADONA-HUB.png"
+                alt="Logo de Bonnadona HUB"
+                style={{ height: 100, marginBottom: 30 }}
+              />
+            </div>
+            <Title
+              style={{
+                textAlign: "center",
+                fontWeight: "normal",
+                lineHeight: 1.3,
+                marginBottom: "2rem",
+              }}
+              level={2}
+            >
+              Iniciar sesión
+            </Title>
+            <Form onFinish={handleSubmit}>
+              <Form.Item
+                name="email"
+                rules={[
+                  { required: true, message: "Por favor ingrese su correo" },
+                  {
+                    pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                    message: "Por favor ingrese un correo válido",
+                  },
+                ]}
+              >
+                <Input
+                  className="custom-input"
+                  type="email"
+                  placeholder="Correo"
+                  autoComplete="off"
+                  prefix={<UserOutlined style={{ color: "rgba(0,0,0,.25)" }} />}
+                  value={email}
+                  style={{ borderRadius: "30px" }}
+                  onChange={handleEmailChange}
+                />
+              </Form.Item>
+              <Form.Item
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: "Por favor ingrese su contraseña",
+                  },
+                  // { pattern: /^[0-9]+$/, message: "El correo es incorrecto" },
+                ]}
+              >
+                <Input.Password
+                  className="custom-input"
+                  prefix={<KeyOutlined style={{ color: "rgba(0,0,0,.25)" }} />}
+                  value={password}
+                  placeholder="Contraseña"
+                  style={{ borderRadius: "30px" }}
+                  onChange={handlePasswordChange}
+                />
+              </Form.Item>
+              <Form.Item>
+                <a href="#">¿Olvidaste tu contraseña?</a>
+              </Form.Item>
+              <Form.Item style={{ textAlign: "center" }}>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  className="login-button"
+                  style={{
+                    borderRadius: "30px",
+                    textAlign: "center",
+                    backgroundColor: "#0085c8",
+                  }}
+                >
+                  Iniciar Sesión
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+        </Col>
+      </Row>
       <TwoFactorAuthModal
         visible={isModalVisible}
         onClose={handleModalClose}
