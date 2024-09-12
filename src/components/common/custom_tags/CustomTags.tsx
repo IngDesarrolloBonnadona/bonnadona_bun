1819af980ac8f82c7e7cf0f6042ac4041562e807
IngDesarrolloBonnadona <ingdesarrollo@bonnadona.co>
2024-09-12 18:06:19 -0500

    custom tags are improved

--- src/components/common/custom_tags/CustomTags.tsx
+++ src/components/common/custom_tags/CustomTags.tsx
@@ -1,21 +1,40 @@
 "use client";
 
 import React from "react";
-
 import { Tag } from "antd";
 
-const CustomTags: React.FC<{ tag: CustomTag }> = ({ tag }) => {
+interface CustomTag {
+  idCustom: string;
+  classNameCustom: string;
+  labelCustom: string;
+  numberCustom?: number;
+  colorCustom: string;
+  iconCustom?: React.ReactNode;
+  stylesCustom?: React.CSSProperties;
+}
+
+const CustomTags: React.FC<CustomTag> = ({
+  idCustom,
+  classNameCustom,
+  labelCustom,
+  numberCustom,
+  colorCustom,
+  iconCustom,
+  stylesCustom,
+}) => {
   return (
     <Tag
-      key={tag.label}
-      color={tag.color}
-      style={{
-        color: tag.textColor,
-        margin: "0px",
-        paddingInline: "13px",
-      }}
+      id={idCustom}
+      className={classNameCustom}
+      key={labelCustom}
+      color={colorCustom}
+      style={{ ...stylesCustom }}
     >
-      {tag.label}
+      <div style={{ display: "flex", gap: "10px" }}>
+        {iconCustom}
+        {labelCustom}
+        {numberCustom}
+      </div>
     </Tag>
   );
 };
