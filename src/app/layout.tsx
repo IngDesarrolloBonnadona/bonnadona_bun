95138a8f123383656423152e89fc6887fb9bcf70
IngDesarrolloBonnadona <ingdesarrollo@bonnadona.co>
2024-09-14 10:53:25 -0500

    add image ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,13 +10,22 @@ export const metadata: Metadata = {
   title: "Bonnadona Hub",
   description: "Bonnadona Hub software by BonnaTics",
   icons: {
-    icon: "favicon.ico",
+    icon: "../../public/logos/favicon.ico",
   },
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en" style={{ margin: 0, padding: 0 }}>
+      <head className="container-head-app">
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+      </head>
       <body className={inter.className} style={{ margin: 0, padding: 0 }}>
         <main className="container">
           <AntdRegistry>{children}</AntdRegistry>
